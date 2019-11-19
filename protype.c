c8a8f0643a8c1b0c2314378e2271838d4fc37e5e
Noah172 <jsebastian.garcia1995@gmail.com>
2019-11-19 22:19:34 +0000

    protype of shell

---
+++ protype.c
@@ -0,0 +1,27 @@
+#include "header.h"
+int main(void)
+{
+	char *line, *tok, *av[100];
+	int c = 0;
+	size_t buf = 0;
+
+	while (1)
+	{
+	write(1, "$ ", 2);
+	if (getline(&line, &buf, stdin) == -1)
+	{
+		write(1, "\n", 1);
+		exit(0);
+	}
+	tok = strtok(line, " \n\t");
+	while (tok != NULL)
+	{
+		av[c] = tok;
+		tok = strtok(NULL, " \n\t");
+		//printf("%s\n", *(av + c));
+		c++;
+	}
+	
+	}
+	return (0);
+}
