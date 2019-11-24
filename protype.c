95b82aab9cfc111155ff519c28c18c1b3d33a450
Noah172 <jsebastian.garcia1995@gmail.com>
2019-11-24 19:31:23 +0000

    putting exect inside the prototype .c file

--- protype.c
+++ protype.c
@@ -1,28 +1,65 @@
 #include "header.h"
-int main(void)
+int main(int ac, char *av[], char *envp[])
 {
-	char *line, *tok, *av[100];
+	if (ac > 1)
+	{
+	}
+	else
+	{
+		rtl(ac, av, envp);
+	}
+
+	return (0);
+}
+
+int rtl(int ac, char *av[], char *envp[])
+{
+	char *line, *tok;
 	int c = 0;
 	size_t buf = 0;
 
 	while (1)
 	{
-	write(1, "$ ", 2);
-	if (getline(&line, &buf, stdin) == -1)
-	{
-		write(1, "\n", 1);
-		exit(0);
+		write(1, "$ ", 2);
+		if (getline(&line, &buf, stdin) == -1)
+		{
+			write(1, "\n", 1);
+			exit(0);
+		}
+		tok = strtok(line, " \n\t");
+		while (tok != NULL)
+		{
+			av[c] = tok;
+			tok = strtok(NULL, " \n\t");
+			c++;
+		}
+		av[c] = NULL;
+		exect(av, envp);
 	}
-	tok = strtok(line, " \n\t");
-	while (tok != NULL)
-	{
-		av[c] = tok;
-		tok = strtok(NULL, " \n\t");
-//		printf("%s\n", *(av + c));
-		c++;
-	}
-	exect(av);
-	}
-
 	return (EXIT_SUCCESS);
+}
+
+int exect(char **args, char *envp[])
+{
+	pid_t pid;
+	int sts;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(args[0], args, envp) == -1)
+		{
+			perror("lpm");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		perror("lpm2");
+	}
+	else
+	{
+		wait(&sts);
+	}
+	return (0);
 }
